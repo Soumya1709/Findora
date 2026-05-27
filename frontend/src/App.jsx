5b1499bae2386220048b10c1a3b3783bfb52cb80
Soumya1709 <soumyaranchi05@gmail.com>
2026-05-27 22:36:07 +0530

    Initial frontend setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-blue-50 flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-blue-600">
+        Findora
+      </h1>
+    </div>
+  );
+}
+
+export default App;
